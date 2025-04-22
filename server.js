bc564cce2b6bf61d438dc3214e0a18b9b25f9d28
ADORA-ATAYA <pragyanshutayal2100@gmail.com>
2025-04-22 22:15:05 +0530

    socket error

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ let rooms = {};
 
 app.use(cors(
   {
-    origin: "https://xstreamfrontend.onrender.com", // restrict calls to those this address
+    origin: "http://xstreamfrontend.onrender.com", // restrict calls to those this address
     credentials: true,
   }
 )) // middleware present b/w client and server to change requests
